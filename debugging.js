343781c70f544e8aa655a92baac8f742acb9e7d1
helamancarrillo <helamancarrillo11@gmail.com>
2024-01-13 22:44:28 -0600

    first week

---
+++ debugging.js
@@ -0,0 +1,14 @@
+/*const PI = 3.14;
+let area = 0;
+
+function circleArea(radius) {
+    const area = radius * PI;
+    return area
+}
+
+area = circleArea(3); 
+console.log(area); */
+
+let num1 = 100;
+let num2 = '10';
+let sum =+ num2 + num1;
